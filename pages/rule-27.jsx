507e62caaba131259c29845c79614d31d7693529
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2022-07-08 15:08:41 -0400

    Added code

---
+++ pages/rule-27.jsx
@@ -0,0 +1,16 @@
+export default function Rule27() {
+    return (
+        <div className='border-y-rose-700'>
+            <h1 className='text-center text-violet-600 font-black text-2xl pt-24'>
+                Palentier Felt like it.. cry liberal :)
+            </h1>
+              <button className='w-max px-6 py-2 text-xs text-mtxWhite leading-6 text-center bg-violet-800 rounded-full shadow ripple hover:shadow-lg hover:bg-violet-700 focus:outline-none'>
+              <a href="/"> Take me back home </a>
+           </button>
+
+           <p className='text-black'>
+               Effort put into the page = 2174234798123047231894712398473128941234141341324%.. Infact this only took me 15 seconds to make. Yes, I want a cookie.
+           </p>
+           </div >
+    )
+}
